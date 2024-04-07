35264ccd196c59adb2e0a3abeb1901837b853bd0
dazzel3 <aakk9350@naver.com>
2024-04-07 18:46:33 +0900

    feat: login with google auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../firebase';
 
 export default function Header() {
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <header className='w-full p-4 px-8 flex justify-between items-center border-b border-gray-800 text-base'>
       <Link to='/products' className='px-4 py-2'>
@@ -16,7 +30,9 @@ export default function Header() {
       <Link to='/products/new' className='px-4 py-2'>
         New
       </Link>
-      <button className='px-4 py-2'>Login</button>
+      <button className='px-4 py-2' onClick={handleGoogleLogin}>
+        Login
+      </button>
     </header>
   );
 }
