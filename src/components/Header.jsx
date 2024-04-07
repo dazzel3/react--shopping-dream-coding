86725384f7eb6f75acaceee9d9c646c21b284ac0
dazzel3 <aakk9350@naver.com>
2024-04-07 17:48:55 +0900

    refactor: remove button and style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='w-full p-4 px-8 flex justify-between items-center border-b border-gray-800'>
-      <Link to='/products'>
-        <button className='text-base rounded-md px-4 py-2'>Products</button>
+    <header className='w-full p-4 px-8 flex justify-between items-center border-b border-gray-800 text-base'>
+      <Link to='/products' className='px-4 py-2'>
+        Products
       </Link>
       <Link to='/'>
         <img className='w-32 h-full' src='/images/logo.png' alt='core logo' />
       </Link>
-      <Link to='/carts'>
-        <button className='text-base rounded-md px-4 py-2'>Carts</button>
+      <Link to='/carts' className='px-4 py-2'>
+        Carts
       </Link>
-      <Link to='/products/new'>
-        <button className='text-base rounded-md px-4 py-2'>New</button>
+      <Link to='/products/new' className='px-4 py-2'>
+        New
       </Link>
-      <button className='text-base rounded-md px-4 py-2'>Login</button>
+      <button className='px-4 py-2'>Login</button>
     </header>
   );
 }
