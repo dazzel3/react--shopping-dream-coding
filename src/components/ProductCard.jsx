e316679d38f2d752f78541c473368b27e80a2ab5
dazzel3 <aakk9350@naver.com>
2024-05-19 13:02:08 +0900

    feat: create ProductCard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-export default function ProductCard({
-  product: { id, image, title, category, price },
-}) {
+export default function ProductCard({ product: { image, title, price } }) {
+  const filteredPrice = useMemo(() => Number(price).toLocaleString(), [price]);
+
   return (
-    <li>
-      <img src={image} alt={title} />
-      <div>
-        <h3>{title}</h3>
-        <p>{`₩${price}`}</p>
+    <li className='overflow-hidden cursor-pointer'>
+      <img className='w-full' src={image} alt={title} />
+      <div className='mt-2 px-2 text-md'>
+        <h3 className='text-stone-800 font-semibold'>{title}</h3>
+        <p className='text-stone-600'>{`KRW ${filteredPrice}`}</p>
       </div>
-      <p>{category}</p>
     </li>
   );
 }
